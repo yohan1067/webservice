395e9826c753f0aaee86b1ff6b5bb0a2b6651b23
yohan.choi <yohan.choi@lemonhc.com>
2019-11-22 12:29:31 +0900

    33

--- src/main/java/com/toy/web/WebRestController.java
+++ src/main/java/com/toy/web/WebRestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebRestController {
     @GetMapping("/hello")
     public String hello() {
-        return "HelloWorld!!";
+        return "HelloWorld!!!";
     }
 }
